5524e265cfef31364baf6d52168af104c454261f
aselshailoobekova <aselkasalbarova@gmail.com>
2024-05-14 23:44:57 +0300

    init

--- simplewin.py
+++ simplewin.py
@@ -1,14 +1,15 @@
 # программа в стиле GUI
 import tkinter as gui
+from const import *
 
 # создать главное окно программы
 win = gui.Tk()
 
 #создать размеры окна
-win.geometry('800x600')
+win.geometry(INIT_WIN_SIZE)
 
 #задать заголовок
-win.title('Грандиозная программа в стиле GUI')
+win.title(MAIN_TITLE)
 
 #запустить окно (программу)
 win.mainloop()
